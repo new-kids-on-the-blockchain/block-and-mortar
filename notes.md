8938c48e1b69eeecb8f69038305da2b55f30ae1c
Annabel Lau <annabel.n.lau@gmail.com>
2018-03-19 18:26:24 -0400

    everyone please use this version - updated contract.js

--- notes.md
+++ notes.md
@@ -7,7 +7,7 @@
 
 var barterAgreement = BarterAgreement.at(BarterAgreement.address)
 
-barterAgreement.newAgreement(0xda9b1a939350dc7198165ff84c43ce77a723ef73, 0xed562f879d4c35304fd9db80155de44513ac35f9, 'Apples and Oranges', 'One hour of dog walking')
+barterAgreement.newAgreement(.5)
 
 barterAgreement.getAgreement(0)
 
--- src/store/contract.js
+++ src/store/contract.js
@@ -3,7 +3,7 @@ import BarterAgreement from '../../build/contracts/BarterAgreement.json';
 /**
  * INITIAL STATE
  */
-const currentContract = {}
+const defaultContract = {}
 
 /**
  * ACTION TYPES
@@ -18,23 +18,17 @@ const setContract = contract => ({type: GET_CONTRACT, contract})
 /**
  * THUNK CREATORS
  */
-export const fetchContract = (web3, id)  => {
+export const fetchContract = web3  => {
   const contract = require('truffle-contract');
-  const barterAgreement = contract(BarterAgreement); //takes our contract and wraps it to give us access to helper functions
-  barterAgreement.setProvider(web3.currentProvider) //sets our contract's Web3 provider to match the state's web3 provider
-  return dispatch => barterAgreement.deployed() //deployed() creates an instance of NoteOwnershipContract that represents the default address managed by NoteOwnershipContract
-    .then(contract => {
-      return contract.getAgreement(id)
-    })
-    .then(returnedContract => {
-      dispatch(setContract(returnedContract)) //then we dispatch our action to our reducer with the returned contract
-    })
+  const simpleStorage = contract(BarterAgreement);
+  simpleStorage.setProvider(web3.currentProvider)
+  return dispatch => simpleStorage.deployed().then(contract => dispatch(setContract(contract)))
 }
 
 /**
  * REDUCER
  */
-export default function (state = currentContract, action) {
+export default function (state = defaultContract, action) {
   switch (action.type) {
     case GET_CONTRACT:
       return action.contract;
