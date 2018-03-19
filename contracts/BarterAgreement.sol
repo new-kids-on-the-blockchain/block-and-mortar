f24732273db2d1cc05e84595b2441787e9546124
Kait Hoehne <kait.hoehne@gmail.com>
2018-03-19 17:03:11 -0400

    working on new version of contract

--- contracts/BarterAgreement.sol
+++ contracts/BarterAgreement.sol
@@ -6,41 +6,39 @@ contract BarterAgreement {
     Agreement[] agreements;
 
     struct Agreement {
-      address userOne;
-      address userTwo;
-      string userOneService;
-      string userTwoService;
-      bool userOneReceivedService;
-      bool userTwoReceivedService;
-      bool completed;
+      uint agreementId
+      address owner;
+      address buyer;
+      uint256 price;
+      bool received;
     }
 
-    // create a new agreement - right now we are creating the contract and passing in user one and user two as arguments. the two users, however, are touching the contract to corroborate services received.
-    function newAgreement(address userOne, address userTwo, string userOneService, string userTwoService) returns (uint id) {
-      id = agreements.push(Agreement(userOne, userTwo, userOneService, userTwoService, false, false, false)) - 1;
-      return id;
+    address public seller
+
+    // create a new agreement
+    function newAgreement(uint256 price) returns (uint agreementId) {
+      agreementId = agreements.push(Agreement(agreements.length-1, msg.sender, null, price, false)) - 1;
+      return agreementId;
     }
-    //this might be returning correctly because we're accessing the storage vs. the interaction itself
+
     // we can't return a struct so we can to return each data value one by one
     function getAgreement(uint agreementID) public view returns (
-      address userOne,
-      address userTwo,
-      string userOneService,
-      string userTwoService,
-      bool userOneReceivedService,
-      bool userTwoReceivedService,
-      bool completed
+      uint agreementId
+      address owner;
+      address buyer;
+      uint256 price;
+      bool received;
     )
     {
-      Agreement storage agreement = agreements[agreementID];
-        return (
-        agreement.userOne,
-        agreement.userTwo,
-        agreement.userOneService,
-        agreement.userTwoService,
-        agreement.userOneReceivedService,
-        agreement.userTwoReceivedService,
-        agreement.completed);}
+      Agreement storage agreement = agreements[agreementID]; //what is Agreement storage agreement doing?
+      return (
+        agreement.agreementId,
+        agreement.owner,
+        agreement.buyer,
+        agreement.price,
+        agreement.received
+      );
+    }
 
     // a fxn that returns the length of the agreements arr so that we can return all of agreements in for loop on the front end:
     function getAgreementLength() returns (uint length) {
@@ -59,4 +57,6 @@ contract BarterAgreement {
       getAgreement(agreementIdx);
     }
   }
+
+
 
