f3f8b896c18a75641af922e98675a4d34747847b
Kait Hoehne <kait.hoehne@gmail.com>
2018-03-22 12:50:49 -0400

    continuing work on new message component

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { postMessage } from '../store';
 
 class AddMessage extends Component {
   constructor() {
     super();
     this.state = {
-      name: ""
+      sender: ""
     }
   }
 
   handleSubmit(evt){
+    //we'll have to check if a thread exists
+    //if it doesn't exist, create the thread
+      //then add message to thread
+    //if it does exist, add message to thread
     evt.preventDefault();
     const formData = {
-      name: evt.target.serviceName.value
+      subject: evt.target.subject.value
     }
   }
 
   render() {
     console.log()
     return (
-
+      <div>
+        <h2>Send New Message</h2>
+        <form>
+          <h3>Subject</h3>
+          <input
+            name="subject"
+            type="text"
+          />
+          <h3>Message</h3>
+          <textarea
+            name="message"
+            type="text"
+          />
+        </form>
+      </div>
     )
   }
 }
