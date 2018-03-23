fb7ce041213eef60c9a09502894e4b3dcc234391
Annabel Lau <annabel.n.lau@gmail.com>
2018-03-23 11:16:55 -0400

    added imgUrl field to form

--- src/components/AddService.js
+++ src/components/AddService.js
@@ -24,6 +24,7 @@ class AddService extends Component {
       category: evt.target.serviceCategory.value,
       price: evt.target.servicePrice.value,
       description: evt.target.serviceDescription.value,
+      imgUrl: evt.target.imgUrl.value,
       contractId: null,
       seller: this.props.currentUser.id //don't hardcode it later
     };
@@ -73,6 +74,8 @@ class AddService extends Component {
             />
             <h3> Description: </h3>
             <textarea name="serviceDescription" rows="1" cols="50" />
+            <h3> Image URL: </h3>
+            <input name="imgUrl" />
             <button> Submit </button>
           </form>
         </div>
