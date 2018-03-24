232a091e09e96878e32df50892d7302032cfa5a8
Annabel Lau <annabel.n.lau@gmail.com>
2018-03-24 16:27:20 -0400

    add username to model attributes

--- server/api/users.js
+++ server/api/users.js
@@ -4,10 +4,10 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   User.findAll({
-    // explicitly select only the id and email fields - even though
+    // explicitly select only the id, username, and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ['id', 'userName', 'email']
   })
     .then(users => res.json(users))
     .catch(next)
