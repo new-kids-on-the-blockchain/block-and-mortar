1575b2be204b30ab8aadc6d43ec85b2d86c3ad4b
manja <anjiemerchant@gmail.com>
2018-03-25 14:53:47 -0400

    minor backend changes

--- server/api/threads.js
+++ server/api/threads.js
@@ -5,24 +5,24 @@ module.exports = router
 
 // this route gets all threads by a specific user, threads on which they are the initiator and threads on which they are the recipient. for loading of the "inbox" component
 router.get('/', (req, res, next) => {
-  Thread.getAllByUser(req.user.id)
+  Thread.getAllByUser(req.user.id
+  )
     .then(threads => res.json(threads))
     .catch(next)
 })
 
 // this route is to find or create a route when a user is the initiator and should be involved in the "current thread thunk"
-router.get('/', (req, res, next) => {
-  Thread.findOrCreate({ where: {InitiatorId: req.user.id}})
-    .then(thread => Thread.findById(thread.id, {include: [{ all: true }]}))
-    .then(foundThread => res.json(foundThread))
-    .catch(next)
-})
-
 router.post('/', (req, res, next) => {
-  Thread.create(req.body)
-    .then(thread => Thread.findById(thread.id, {include: [{ all: true }]}))
-    .then(foundThread => res.json(foundThread))
-    .catch(next)
+  Thread.findOrCreate({ where: {
+      buyerId: req.user.id,
+      sellerId: req.body.sellerId,
+      serviceId: req.body.serviceId
+    }})
+    .spread(function (thread, createdThreadBool) {
+      return Thread.findById(thread.id, {include: [{ all: true }]})
+      .then(foundThread => res.json(foundThread))
+  })
+  .catch(next);
 })
 
 router.get('/:id', (req, res, next) => {
