ef4476dfb68fb8f312046b3b5ae620221b9d9897
manja <anjiemerchant@gmail.com>
2018-03-26 09:29:03 -0400

    minor blockchain language updates

--- public/bundle.js
+++ public/bundle.js
@@ -81146,7 +81146,7 @@ exports.default = (0, _reactRouterDom.withRouter)((0, _reactRedux.connect)(mapSt
 
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.default = Homepage;
 
@@ -81158,30 +81158,40 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function Homepage() {
 
-  return _react2.default.createElement(
-    "div",
-    { className: "avenir" },
-    _react2.default.createElement(
-      "h1",
-      null,
-      "Aimed at local communities, xxxx allows for the grassroots sale of goods and services using the Ethereum blockchain. "
-    ),
-    _react2.default.createElement(
-      "div",
-      { className: "avenir flex items-center justify-center pa4 bg-lightest-blue navy" },
-      _react2.default.createElement(
-        "p",
-        { className: "lh-title ml3" },
-        "Before signing up or logging into xxxx, please make sure you're signed up and logged into your ",
+    return _react2.default.createElement(
+        "div",
+        { className: "avenir" },
         _react2.default.createElement(
-          "a",
-          { href: "https://metamask.io/" },
-          "Metamask account"
+            "h1",
+            null,
+            "Aimed at local communities, xxxx allows for the grassroots sale of goods and services using the Ethereum blockchain. "
         ),
-        ". For those of you new to the Ethereum blockchain, Metamask connects you to the Ethereum blockchain. It also acts like a digital wallet and you'll need a bit of ether cryptocurrency to engage with the blockchain. Once you're set up, you will be able to buy or sell goods and services within your local community, an exchange facilitated by a \"smart contract\". What's a smart contract? Sign up and log in to learn more!"
-      )
-    )
-  );
+        _react2.default.createElement(
+            "div",
+            { className: "avenir flex items-center justify-center pa4 bg-lightest-blue navy" },
+            _react2.default.createElement(
+                "p",
+                { className: "lh-title ml3" },
+                "Before signing up or logging into xxxx, please make sure you've signed up and logged into your ",
+                _react2.default.createElement(
+                    "a",
+                    { href: "https://metamask.io/" },
+                    "Metamask account"
+                ),
+                "."
+            ),
+            _react2.default.createElement(
+                "p",
+                null,
+                "For those of you new to blockchain technology, Metamask connects you to the Ethereum blockchain, a decentralized cryptocurrency platform that serves as a public ledger. Metamask will also be your digital wallet, so you'll need a bit of ether cryptocurrency to engage with your community on xxxxxx."
+            ),
+            _react2.default.createElement(
+                "p",
+                null,
+                "Once you're set up with Metamask and xxxx, you will be able to buy or sell goods and services within your local community, an exchange codified and facilitated by a \"smart contract\". Login to learn more!"
+            )
+        )
+    );
 }
 
 /***/ }),
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,9 +6,11 @@ export default function Homepage () {
        <div className="avenir">
            <h1>Aimed at local communities, xxxx allows for the grassroots sale of goods and services using the Ethereum blockchain. </h1>
             <div className="avenir flex items-center justify-center pa4 bg-lightest-blue navy">
-           <p className="lh-title ml3">Before signing up or logging into xxxx, please make sure you're signed up and logged into your <a href="https://metamask.io/">Metamask account</a>. For those of you new to the Ethereum blockchain, Metamask connects you to the Ethereum blockchain. It also acts like a digital wallet and you'll need a bit of ether cryptocurrency to engage with the blockchain.
+                <p className="lh-title ml3">Before signing up or logging into xxxx, please make sure you've signed up and logged into your <a href="https://metamask.io/">Metamask account</a>.</p>
 
-           Once you're set up, you will be able to buy or sell goods and services within your local community, an exchange facilitated by a "smart contract". What's a smart contract? Sign up and log in to learn more!</p>
+                <p>For those of you new to blockchain technology, Metamask connects you to the Ethereum blockchain, a decentralized cryptocurrency platform that serves as a public ledger. Metamask will also be your digital wallet, so you'll need a bit of ether cryptocurrency to engage with your community on xxxxxx.</p>
+
+                <p>Once you're set up with Metamask and xxxx, you will be able to buy or sell goods and services within your local community, an exchange codified and facilitated by a "smart contract". Login to learn more!</p>
             </div>
        </div>
     );
