bde711918ecf88c951a243006bbd1d1bc6630395
Claudia Baik <claudiabaik@gmail.com>
2018-03-27 01:16:56 -0400

    home bg added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,18 @@
 import React from "react";
+//import Background from '../../public/assets/bgimg/home.png'
 
-export default function Homepage () {
-
-    return (
-       <div className="avenir">
-           <h1>Aimed at local communities, Block &amp; Mortar allows for the grassroots sale of goods and services using the Ethereum blockchain. </h1>
-       {/*Claudia's background will go somewhere here*/}
-           </div>
-    );
+export default function Homepage() {
+  return (
+    <div className="home" id="background">
+      <div id="homeText" >
+        <h2>
+          Aimed at local communities, Block &amp; Mortar allows for the
+          grassroots sale of goods and services using the Ethereum blockchain.{" "}
+        </h2>
+      </div>
+      <div id="homeImg">
+        <img role="presentation"  src="/assets/bgimg/phone2.png" />
+      </div>
+    </div>
+  );
 }
