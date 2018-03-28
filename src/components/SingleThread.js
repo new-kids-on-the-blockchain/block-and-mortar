ef6e52db5aa36ae3437f1d0da3ef53ad479584d4
Kait Hoehne <kait.hoehne@gmail.com>
2018-03-28 17:27:17 -0400

    css styling to message components

--- src/components/SingleThread.js
+++ src/components/SingleThread.js
@@ -40,34 +40,32 @@ class SingleThread extends Component {
   }
 
   render() {
-    console.log(this.props.currentThread, "CURRENT THREAD!!!!")
-    console.log(this.props.currentUser, "CURRENT USER!!!!")
-    console.log(this.state.messages, "MESSAGES!!!!")
     const currentThread = this.props.currentThread
     const buyerId = currentThread.buyerId
     const sellerId = currentThread.sellerId
     const messages = this.state.messages
 
     return (
-      <div className="avenir">
+      <div className="avenir ph4 fl w-60 bl">
         {!this.props.currentThread.id ? (
-          <div>No Conversation Selected</div>
+          <div className="f3">No Conversation Selected</div>
         ) : !this.state.messages.length ? (
           <div>
-            <div>No Messages in this Conversation</div>
+            <div className="f3">No Messages in this Conversation</div>
             <div>
               <AddMessage currentThread={this.props.currentThread} />
             </div>
           </div>
         ) : (
-          <div>
+          <div className="pa3">
+            <div className="f3">Selected Conversation</div>
             {this.state.messages && this.state.messages.map(message => {
               return (
-                <div className="message" key={message.id}>
-         
-                <b>{message.senderId === this.props.currentThread.buyerId ? (<b>{this.props.currentThread.buyer.userName}: </b>) 
-                
-                  : (<b>{this.props.currentThread.seller.userName}: </b>)}</b>
+                <div className="message pa2" key={message.id}>
+
+                <span className="pa3">{message.senderId === this.props.currentThread.buyerId ? (<span className="b">{this.props.currentThread.buyer.userName}: </span>)
+
+                  : (<span className="b">{this.props.currentThread.seller.userName}: </span>)}</span>
 
                   {message.content}
                 </div>
