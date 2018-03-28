740425338822199cbad0c97f4ed157a394e244c4
Claudia Baik <claudiabaik@gmail.com>
2018-03-28 00:37:48 -0400

    added modal

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,23 @@
 import React from "react";
 //import Background from '../../public/assets/bgimg/home.png'
+import {Signup} from './SignUpModal'
 
 export default function Homepage() {
   return (
     <div className="home" id="background">
       <div id="homeText">
-        <div className="f1">
-          Aimed at local communities,<br/><span className="b">Block &amp; Mortar</span> allows for the
+        <div  id="text">
+          Aimed at local communities,<br/><span className="b f1 lh-title white">Block &amp; Mortar</span> allows for the
           grassroots sale of goods and services using the Ethereum blockchain.
         </div>
+        <Signup className="modal"/>
       </div>
       <div id="homeImg">
         <img role="presentation"  src="/assets/bgimg/phone2.png" />
+
       </div>
+
+
     </div>
   );
 }
